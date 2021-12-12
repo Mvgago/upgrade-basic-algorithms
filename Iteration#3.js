33d66b6d6aa978149f0d4e4780e57d9765f2dc64
Mvgago <mvgago26@gmail.com>
2021-12-12 20:52:48 +0100

    Ejercicios JS Basic Algorithms

---
+++ Iteration#3.js
@@ -0,0 +1,36 @@
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+let a = 10;
+let b = 5;
+let total = 0;
+total = a * b;
+//alert(total)
+
+
+//1.2 Divide 10 por 2 y muestra el resultado en un alert.
+let c = 10;
+let d = 2;
+let resultado = 0;
+resultado = c / d;
+//alert(resultado)
+
+
+//1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+let e = 15;
+let f = 9;
+let resultado = 0;
+resultado1 = e / f;
+alert(resultado1)
+
+//1.4 Usa el correcto operador de asignación que resultará en x = 15, teniendo dos variables y = 10 y z = 5.
+
+let y = 10;
+let z = 5;
+let x = y + z;
+
+//1.5 Usa el correcto operador de asignación que resultará en x = 50, teniendo dos variables y = 10 y z = 5.
+let y = 10;
+let z = 5;
+let x = y * z;
+
+
+
